0ad61699259c03d5ad66646def5ecfce05990a22
giovantarunad <giovantarunad@gmail.com>
2020-03-18 22:34:18 +0700

    nyicil project uts

--- app/src/main/java/com/example/travel_giovantarunad/MainActivity.java
+++ app/src/main/java/com/example/travel_giovantarunad/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.travel_giovantarunad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void profileMenu(View v) {
+        Intent i = new Intent(this, ProfileActivity.class);
+        startActivity(i);
+    }
+
 }
